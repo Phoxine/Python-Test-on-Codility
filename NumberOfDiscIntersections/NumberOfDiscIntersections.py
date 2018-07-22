e786c911eaded2136bad70ee419b0d9344c63858
Phoxine <kiraychao@gmail.com>
2018-07-22 13:02:18 +0800

    optimize the process
    still performance problem

--- NumberOfDiscIntersections/NumberOfDiscIntersections.py
+++ NumberOfDiscIntersections/NumberOfDiscIntersections.py
@@ -6,15 +6,19 @@ def solution(A):
     #圓數量為0,1 無相交
     if N<2:
         return 0;
-    for first in range(N-1):
+    for first in range(N-1):   
         second=first+A[first]
-        counter+=A[first]
-        for second in range(first+1,N):
-
+        #圓心(first)座標+半徑 > 圓心(N-1)-圓心(first) 長度取後者
+        if second-first>(N-1)-first:
+            counter+=(N-1)-first
+        else:
+            counter+=second-first   
+        for second in range(second+1,N):
             if second-first <= A[first]+A[second]:
                 counter+=1;
-    #相交次數大於1e7 回傳1
-    if counter>10000000:
-                    return -1            
+            #相交次數大於1e7 回傳1
+            if counter>10000000:
+                return -1
+        #print(first,counter)
     return counter
     pass
