d8138d1c41ee9dd73c7eab9f1dbca3c260f98a99
Phoxine <kiraychao@gmail.com>
2018-07-27 12:15:30 +0800

    description

--- Brackets/Brackets.py
+++ Brackets/Brackets.py
@@ -6,7 +6,7 @@ def solution(S):
     stack=[]
     #以串列S首項為iteration做迴圈
     for character in S:
-        #當character為 dict(left)的其中一項 push() 
+        #當character為 dict(left)的其中一項 push character對應的數字
         if character in left:
             stack.append(left[character])
         #當stack為空串列(if not x->x為false執行後面指令 true則否) 或是 左右括號dict對應的值不同 return 0   
